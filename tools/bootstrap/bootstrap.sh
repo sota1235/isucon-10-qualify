a64acad682626e2d64d22ea234f1fef59bb44621
TKNGUE <takegue@gmail.com>
2020-09-11 23:59:55 +0900

    Add bootstrap tools for initial setup (#3)

---
+++ tools/bootstrap/bootstrap.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+## Install Tools
+apt update && apt install -y \
+  bash-completion \
+  curl \
+  dnsutils \
+  dstat \
+  git \
+  htop \
+  jq \
+  net-tools \
+  nginx \
+  silversearcher-ag \
+  tcpdump \
+  unzip \
+  vim \
+  wget \
+  tree
