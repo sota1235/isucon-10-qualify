cb570e5c730d461cb9f0838d2b684c3df3c902eb
sota1235 <sota1235@gmail.com>
2020-09-12 18:08:54 +0900

    fix

--- isuumo/webapp/go/main.go
+++ isuumo/webapp/go/main.go
@@ -206,9 +206,9 @@ func NewMySQLConnectionEnv() *MySQLConnectionEnv {
 	return &MySQLConnectionEnv{
 		Host:     getEnv("MYSQL_HOST", "127.0.0.1"),
 		Port:     getEnv("MYSQL_PORT", "3306"),
-		User:     getEnv("MYSQL_USER", "root"),
+		User:     getEnv("MYSQL_USER", "isucon"),
 		DBName:   getEnv("MYSQL_DBNAME", "isuumo"),
-		Password: getEnv("MYSQL_PASS", ""),
+		Password: getEnv("MYSQL_PASS", "isucon"),
 	}
 }
 
